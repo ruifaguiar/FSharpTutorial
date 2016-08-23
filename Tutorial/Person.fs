a9716c4f77d09a0d5185c6fe80e316b04546fa71
Rui Aguiar <rui.aguiar@faroeurope.com>
2016-08-23 18:04:28 +0100

    Added Tutorials from TryFSharp.org

---
+++ Tutorial/Person.fs
@@ -0,0 +1,20 @@
+﻿namespace Persons
+
+module Person=
+    type Person(name:string, age:int)=
+        let mutable internalAge = age
+   
+        new (name:string)=Person(name,0)
+
+        member this.Name=name
+
+        member this.Age
+            with get()=internalAge
+            and set(value)=internalAge<-value
+    
+        member this.HasBirthday () = internalAge<- internalAge+1
+
+        member this.IsOfAge targetAge=internalAge>=targetAge
+
+        override this.ToString()=
+            "Name: "+name + " Age: "+ string(internalAge)
--- Tutorial/Program.fs
+++ Tutorial/Program.fs
@@ -1,4 +1,6 @@
-﻿//Square
+﻿
+
+//Square
 let anInt = 5
 let square n = n * n
 //Call the function to calculate the square of a number
@@ -19,10 +21,43 @@ printfn "%O" turnChoices
 
 //List
 let friends = ["João";"Pedro";"Rita";"Berta";"Raquel"]
-printfn "My friends list are: %O" friends
+printfn "My friends list are: %A" friends
 //add element to the head of the list
 let newFriends = "Luisa" :: friends
-printfn "My new friends list is %O" newFriends
+printfn "My new friends list is %A" newFriends
+
+let firstHundred = [0..100]
+let doubledList = List.map(fun x->x*2)firstHundred
+printfn "The new List is %A" doubledList
+let evenList = List.filter(fun x->x%2=0)doubledList
+printfn "The Module List is %A" (evenList)
+
+//Pipe Operator
+let resultWithPipeOperator=
+    [0..100]
+    |>List.map(fun x->x*2)
+    |>List.filter(fun x->x%2=0)
+printfn "The result of pipe operator is %A" resultWithPipeOperator
+
+//using Class and namespace located in another file
+let p1 = Persons.Person.Person("John",43)
+let p2 = Persons.Person.Person("Carie")
+p2.Age<-15
+p2.HasBirthday()
+p1.HasBirthday()
+System.Console.WriteLine(p1.ToString())
+System.Console.WriteLine(p2.ToString())
+
+//Lambda Functions
+let add = (fun x y -> x+y)
+printfn "the add of 2+2 is %d" (add 2 2)
+
+//Passing a anonymous function to twotest function
+let twotest test = 
+    test 2
+printfn "Is 2 > 0? %b"( twotest (fun x-> x > 0))
+
+
 
 
 
